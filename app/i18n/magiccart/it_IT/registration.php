6c91bca80058d6e9b08835b820e31061bb12fb50
Nguyen Dong <team.magiccart@gmail.com>
2017-03-01 11:45:45 +0700

    de_DE, es_ES, fr_FR, it_IT, ru_RU, sv_SE

---
+++ app/i18n/magiccart/it_IT/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magiccart_it_it',
+    __DIR__
+);
---
+++ app/i18n/magiccart/ru_RU/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magiccart_ru_ru',
+    __DIR__
+);
